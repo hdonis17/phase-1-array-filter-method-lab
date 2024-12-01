8bafe4a72e953d5989daadc27c69edd4099bdb3f
Harrison Donis <hdonis17@gmail.com>
2024-12-01 16:11:51 -0500

    completes lab

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solution here
+const driversNames = ["Jim", "Steve", "Larry", "David", "John"];
+const driverObj = [{name: "Jim", place: "Florida"}, {name: "Steve", place: "NY"}]
+
+
+function findMatching(array, string){
+    return array.filter(name => {
+        return name.toLowerCase() === string.toLowerCase();
+    });
+};
+
+function fuzzyMatch(array, string){
+    return array.filter(name => {
+        return name[0] === string[0]
+    });
+};
+
+function matchName(array, string){
+    return array.filter(object => {
+        return object["name"] === string
+    });
+};
